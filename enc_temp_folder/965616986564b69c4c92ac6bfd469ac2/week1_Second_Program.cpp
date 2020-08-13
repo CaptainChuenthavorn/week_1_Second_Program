c778751e12e8fbeb927e4e231835474235e84357
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-08-13 10:53:02 +0700

    Add project files.

---
+++ enc_temp_folder/965616986564b69c4c92ac6bfd469ac2/week1_Second_Program.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int i, j, k, n;
+	printf("Enter number : ");
+	scanf_s("%d", &n);
+	for (i = 0;i < 2 * n;i++) {
+		for (j = 0;j < n - i;j++)
+		{
+			printf(" ");
+		}
+		for (k = 0;k < i;j++);
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+	/*for (i = 0;i < (n-1);i++) {
+		for (j = 0;j < i;j++)
+		{
+			printf(" ");
+		}
+		for (k = 0;k < (n - i) ; j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+}*/
+	return 0;
+}
